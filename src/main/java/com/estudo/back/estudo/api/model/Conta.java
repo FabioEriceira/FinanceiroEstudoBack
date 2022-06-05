d49e8c7b444398484acad375286cb7c403ff45de
FabioEriceira <ftericeira@gmail.com>
2022-06-05 11:02:03 -0300

    mapeando as entidades com JPA
    mapeando as entidades com JPA

--- src/main/java/com/estudo/back/estudo/api/model/Conta.java
+++ src/main/java/com/estudo/back/estudo/api/model/Conta.java
@@ -1,12 +1,22 @@
 package com.estudo.back.estudo.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Embeddable
 public class Conta {
+	@Column(name = "conta_numero")
 	private Long numero;
+	
+	@Column(name = "conta_saldo")
 	private Double saldo;
 	
 	
