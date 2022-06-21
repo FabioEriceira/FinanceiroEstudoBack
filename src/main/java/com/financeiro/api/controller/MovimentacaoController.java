6ca8aaf71d1357f5f89465a2106d8094ae4bfcef
FABIO T. ERICEIRA <96257601+FabioEriceira@users.noreply.github.com>
2022-06-21 15:26:41 -0300

    Versão final projeto
    Versão final projeto

--- src/main/java/com/financeiro/api/controller/MovimentacaoController.java
+++ src/main/java/com/financeiro/api/controller/MovimentacaoController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.financeiro.api.dto.NovaMovimentacao;
-import com.financeiro.api.dto.NovoCorrentista;
 import com.financeiro.api.model.Movimentacao;
 import com.financeiro.api.repository.MovimentacaoRepository;
 import com.financeiro.api.service.MovimentacaoService;
@@ -33,6 +33,11 @@ public class MovimentacaoController {
 	public void save (@ RequestBody NovaMovimentacao movimentacao) {
 		service.save(movimentacao);
 		
+	}
+	
+	@GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findByIdConta(idConta);
 	}
 	
 }
--- src/main/java/com/financeiro/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/financeiro/api/repository/MovimentacaoRepository.java
@@ -1,9 +1,14 @@
 package com.financeiro.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.financeiro.api.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>{
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
+	
 
 }
