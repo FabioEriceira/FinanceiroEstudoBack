19cddab0a22be3542c5f3b196c089c0d01d4ef2e
FABIO T. ERICEIRA <96257601+FabioEriceira@users.noreply.github.com>
2022-07-03 11:02:21 -0300

    Enviando para o git

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Estudo {
+	exports com.financeiro.api.controller;
+	exports com.financeiro.api.dto;
+	exports com.financeiro.api.service;
+	exports com.financeiro.api.model;
+	exports com.financeiro.api;
+	exports com.financeiro.api.repository;
+	exports com.financeiro.api.config;
+}
